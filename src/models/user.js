d37ff596a3abfc62814b3ee0eb4dd1f42c49c37c
Julian <julian.fm@live.com>
2022-01-28 17:12:23 -0300

    actualizacion

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+let Schema = mongoose.Schema;
+export default class User{
+    constructor(data){
+        this.data=data;
+    }
+    static get model(){
+        return 'Users';
+    }
+    static get schema(){
+        return {
+            first_name:{
+                type:String,
+                required:true,
+            },
+            last_name:{
+                type:String,
+                required:true,
+            },
+            age:{
+                type:Number
+            },
+            username:{
+                type:String,
+                default:"anonymus",
+                unique:true
+            },
+            email:{
+                type:String,
+                required:true,
+                unique:true
+            },
+            password:{
+                type:String,
+                required:true
+            }
+        }
+    }
+}
---
+++ src/services/users.js
@@ -0,0 +1,11 @@
+import User from "../models/user.js";
+import GenericQueries from "./genericQueries.js";
+
+export default class UserService extends GenericQueries{
+    constructor(dao){
+        super(dao,User.model);
+    }
+    async findByUsername(username){
+        return this.dao.findOne({username},User.model);
+    }
+}
